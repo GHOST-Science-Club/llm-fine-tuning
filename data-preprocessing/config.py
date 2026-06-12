8e561aa9f493bb2aac7c61933c2de2a5bbab11ea
Eryk Bielawski <erybie222@gmail.com>
2026-06-12 21:15:10 +0200

    fix(pipeline): change DATA_DIR ,SAVE_LOGS, PUSH_TO_HUB AND LOAD_FROM_HUB to local variables

--- data-preprocessing/config.py
+++ data-preprocessing/config.py
@@ -14,11 +14,11 @@ class PipelineConfig:
     BASE_URL: str = field(default_factory=lambda: os.getenv("PCSS_BASE_URL", "https://llm.hpc.psnc.pl/v1/chat/completions"))
     MODEL: str = field(default_factory=lambda: os.getenv("MODEL", "llama3.3:70b"))
     DEBUG: bool = field(default_factory=lambda: os.getenv("DEBUG", "false") == "true")
-    LOAD_FROM_HUB: bool = field(default_factory=lambda: os.getenv("LOAD_FROM_HUB", "false") == "true")
-    PUSH_TO_HUB: bool = field(default_factory=lambda: os.getenv("PUSH_TO_HUB", "false") == "true")
-    SAVE_LOGS: bool = field(default_factory=lambda: os.getenv("SAVE_LOGS", "true") == "true")
+    load_from_hub: bool = field(default_factory=lambda: os.getenv("LOAD_FROM_HUB", "false") == "true")
+    push_to_hub: bool = field(default_factory=lambda: os.getenv("PUSH_TO_HUB", "false") == "true")
+    save_logs: bool = field(default_factory=lambda: os.getenv("SAVE_LOGS", "true") == "true")
 
-    DATA_DIR: Path = field(default_factory=lambda: _MODULE_PATH / "data")
+    data_dir: Path = field(default_factory=lambda: _MODULE_PATH / "data")
 
     INPUT_FILE: Path = field(init=False)
     OUTPUT_FILE: Path = field(init=False)
@@ -35,13 +35,13 @@ class PipelineConfig:
     SEED: int = 42
 
     def __post_init__(self) -> None:
-        self.INPUT_FILE = self.DATA_DIR / "input" / "forum_example_fixed.jsonl"
-        self.OUTPUT_FILE = self.DATA_DIR / "output" / "pipeline_output.jsonl"
-        self.LOG_FILE = self.DATA_DIR / "logs" / "pipeline_logs.jsonl" if self.SAVE_LOGS else None
-        self.DATASET_FILE = self.DATA_DIR / "dataset" / "pipeline_output.jsonl"
-        self.CHECKPOINT_FILE = self.DATA_DIR / "checkpoint" / "checkpoint.txt"
-        self.INPUT_SOURCE = "meta-math/MetaMathQA" if self.LOAD_FROM_HUB else self.INPUT_FILE
-        self.DATASET_DESTINATION = "erybie222/test" if self.PUSH_TO_HUB else self.DATASET_FILE
+        self.INPUT_FILE = self.data_dir / "input" / "forum_example_fixed.jsonl"
+        self.OUTPUT_FILE = self.data_dir / "output" / "pipeline_output.jsonl"
+        self.LOG_FILE = self.data_dir / "logs" / "pipeline_logs.jsonl" if self.save_logs else None
+        self.DATASET_FILE = self.data_dir / "dataset" / "pipeline_output.jsonl"
+        self.CHECKPOINT_FILE = self.data_dir / "checkpoint" / "checkpoint.txt"
+        self.INPUT_SOURCE = "meta-math/MetaMathQA" if self.load_from_hub else self.INPUT_FILE
+        self.DATASET_DESTINATION = "erybie222/test" if self.push_to_hub else self.DATASET_FILE
         paths = [self.OUTPUT_FILE.parent, self.DATASET_FILE.parent,
                  self.INPUT_FILE.parent, self.CHECKPOINT_FILE.parent]
         if self.LOG_FILE:
